acb26c1badddcca28f32242efc0956d840f4dacf
Laia Navalon <laianavalon@gmail.com>
2024-06-13 19:28:13 +0200

    Start creating Header.tsx component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,62 @@
-function Header() {}
+import { Link, NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="pt-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="flex items-center justify-between font-montserrat w-2/5">
+          <NavLink to="/" className="text-lg font-bold tracking-widest">
+            LAIA NAVALON
+          </NavLink>
+          <NavLink to="/about" className="font-medium">
+            ABOUT
+          </NavLink>
+          <h1 className="font-medium">CONTACT</h1>
+        </div>
+        <div>
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <g clipPath="url(#clip0_429_11066)">
+                {" "}
+                <path
+                  d="M3 6.00092H21M3 12.0009H21M3 18.0009H21"
+                  stroke="#292929"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>{" "}
+              </g>{" "}
+              <defs>
+                {" "}
+                <clipPath id="clip0_429_11066">
+                  {" "}
+                  <rect
+                    width="24"
+                    height="24"
+                    fill="white"
+                    transform="translate(0 0.000915527)"
+                  ></rect>{" "}
+                </clipPath>{" "}
+              </defs>{" "}
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
