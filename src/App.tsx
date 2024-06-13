c3dbb2ab522cf156a7ecafc13728cb0c2161a66f
Laia Navalon <laianavalon@gmail.com>
2024-06-13 19:57:40 +0200

    Create Work.tsx page, create Route and implement global font type property in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Work from "./pages/Work";
 import Commercial from "./pages/Commercial";
 import Personal from "./pages/Personal";
 
 function App() {
   return (
-    <div>
+    <div className="font-montserrat tracking-widest">
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/commercial" element={<Commercial />} />
-        <Route path="/personal" element={<Personal />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/work/commercial" element={<Commercial />} />
+        <Route path="/work/personal" element={<Personal />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/Work.tsx
@@ -0,0 +1,9 @@
+function Work() {
+  return (
+    <div>
+      <h1>This is Work.tsx</h1>
+    </div>
+  );
+}
+
+export default Work;
