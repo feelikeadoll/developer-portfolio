78410d10b5c35abcd0f372196a103b219716363e
Laia Navalon <laianavalon@gmail.com>
2024-06-18 19:32:34 +0200

    Change structure in Intro section in Home.tsx for correct design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,59 @@ function Home() {
     <div className="w-full">
       <section
         id="intro"
-        className="w-full bg-black bg-introPic bg-no-repeat bg-right-top bg-50% text-white h-screen"
+        className="w-full bg-black bg-introPic bg-no-repeat bg-right-top bg-40% text-white h-screen"
       >
-        <div className="max-w-7xl mx-auto z-50 ">
-          <h2 className="text-2xl">PORTFOLIO</h2>
-          <h1 className="text-9xl font-bold">WEB DEV.</h1>
-          <h3 className="text-sm">SEE MY WORKS</h3>
-          <h3 className="text-sm">HI, MY NAME IS LAIA!</h3>
-          <p className="text-2xl">I am a Fullstack Developer</p>
+        <div className="z-50">
+          <div className="max-w-7xl pt-28 h-full flex flex-col items-start justify-center mx-auto ">
+            <div className="w-2/3 pt-20 mb-4">
+              <h2 className="text-2xl -mb-4 lg:-mb-6 xl:-mb-10">PORTFOLIO</h2>
+              <h1 className="text-8xl lg:text-8.5xl xl:text-9xl font-bold">
+                WEB DEV.
+              </h1>
+            </div>
+          </div>
+          <div className="w-full">
+            <hr className="border mb-6" />
+            <div className="max-w-7xl h-full flex flex-col items-start justify-center mx-auto ">
+              <div className="flex items-center">
+                <h3 className="text-md">SEE MY WORKS</h3>
+                <svg
+                  fill="#000000"
+                  width="40px"
+                  height="40px"
+                  viewBox="0 0 24 24"
+                  id="right-arrow"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon line"
+                  stroke="#000000"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      id="primary"
+                      d="M3,12H21m-3,3,3-3L18,9"
+                      fill="none"
+                      stroke="#ffffff"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <hr className="border mt-6" />
+          </div>
+
+          <div className="pt-16 max-w-7xl h-full flex flex-col items-start justify-center mx-auto ">
+            <h3 className="text-md">HI, MY NAME IS LAIA!</h3>
+            <p className="text-3xl">I am a Fullstack Developer</p>
+          </div>
         </div>
       </section>
       <section id="quality" className="w-full bg-lightgrey text-black h-screen">
