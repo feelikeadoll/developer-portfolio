fcbb70c445d59b61e3f832dd0143119d0020522e
Laia Navalon <laianavalon@gmail.com>
2024-06-18 19:47:24 +0200

    Add buttons and svg icons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import HuetopiaDevices from "../assets/HuetopiaDevices.png";
 
 function Home() {
@@ -17,41 +18,43 @@ function Home() {
             </div>
           </div>
           <div className="w-full">
-            <hr className="border mb-6" />
+            <hr className="border mb-4" />
             <div className="max-w-7xl h-full flex flex-col items-start justify-center mx-auto ">
-              <div className="flex items-center">
-                <h3 className="text-md">SEE MY WORKS</h3>
-                <svg
-                  fill="#000000"
-                  width="40px"
-                  height="40px"
-                  viewBox="0 0 24 24"
-                  id="right-arrow"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon line"
-                  stroke="#000000"
-                >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <path
-                      id="primary"
-                      d="M3,12H21m-3,3,3-3L18,9"
-                      fill="none"
-                      stroke="#ffffff"
+              <Link to="/work">
+                <button className="btn btn-neutral bg-black border-0 text-white">
+                  <h3 className="text-md pr-2">SEE MY WORKS</h3>
+                  <svg
+                    fill="#000000"
+                    width="40px"
+                    height="40px"
+                    viewBox="0 0 24 24"
+                    id="right-arrow"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon line"
+                    stroke="#000000"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      strokeWidth="1.5"
-                    ></path>
-                  </g>
-                </svg>
-              </div>
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        id="primary"
+                        d="M3,12H21m-3,3,3-3L18,9"
+                        fill="none"
+                        stroke="#ffffff"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.5"
+                      ></path>
+                    </g>
+                  </svg>
+                </button>
+              </Link>
             </div>
-            <hr className="border mt-6" />
+            <hr className="border mt-4" />
           </div>
 
           <div className="pt-16 max-w-7xl h-full flex flex-col items-start justify-center mx-auto ">
@@ -71,9 +74,56 @@ function Home() {
                   I specialise in creating innovative web-applications that work
                   from every device to provide the best user experience.
                 </p>
-                <button className="text-white bg-black rounded px-6 py-2">
-                  SEE MY WORKS
-                </button>
+                <Link to="/work">
+                  <button className="btn btn-neutral bg-black text-white">
+                    <h3 className="text-md pr-2">SEE MY WORKS</h3>
+                    <svg
+                      width="30px"
+                      height="30px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <g clip-path="url(#clip0_429_11254)">
+                          {" "}
+                          <path
+                            d="M10 17L15 12"
+                            stroke="#ffffff"
+                            strokeWidth="2.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>{" "}
+                          <path
+                            d="M15 12L10 7"
+                            stroke="#ffffff"
+                            strokeWidth="2.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>{" "}
+                        </g>{" "}
+                        <defs>
+                          {" "}
+                          <clipPath id="clip0_429_11254">
+                            {" "}
+                            <rect
+                              width="24"
+                              height="24"
+                              fill="white"
+                            ></rect>{" "}
+                          </clipPath>{" "}
+                        </defs>{" "}
+                      </g>
+                    </svg>
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
