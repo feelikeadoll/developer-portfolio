a192b536b17979fe0b239ef059dd78d4c4ae6afb
Laia Navalon <laianavalon@gmail.com>
2024-06-26 17:01:36 +0200

    Adjust responsiveness for smaller screens in Header.tsx and Navbar.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,10 +17,10 @@ function Header() {
   };
 
   return (
-    <div className="absolute w-full z-40">
+    <div className="absolute w-full z-40 px-10 xl:px-0">
       <Navbar displayNavbar={displayNavbar} closeNavbar={closeNavbar} />
       <div className="py-4 max-w-7xl mx-auto flex justify-between items-center">
-        <div className="flex items-center w-1/2 text-white">
+        <div className="flex items-center w-full lg:w-3/4 xl:w-1/2 text-white">
           <NavLink to="/" className="text-lg font-bold flex items-center">
             <img
               src={LNlogo}
@@ -29,20 +29,30 @@ function Header() {
             />
             LAIA NAVALON
           </NavLink>
-          <NavLink to="/about" className="hover:font-medium ml-32">
+          <NavLink
+            to="/about"
+            className="hidden md:block hover:font-medium ml-20 xl:ml-32"
+          >
             ABOUT
           </NavLink>
-          <HashLink to="#contact" smooth className="hover:font-medium ml-36">
+          <HashLink
+            to="#contact"
+            smooth
+            className="hidden md:block hover:font-medium ml-24 xl:ml-36"
+          >
             CONTACT
           </HashLink>
         </div>
-        <button onClick={openNavbar} className="btn btn-ghost btn-square">
+        <button
+          onClick={openNavbar}
+          className="btn btn-ghost btn-square hidden md:block"
+        >
           <svg
             width="50px"
             height="50px"
             viewBox="0 0 24 24"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/1000/svg"
           >
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
@@ -57,6 +67,50 @@ function Header() {
                 <path
                   d="M3 6.00092H21M3 12.0009H21M3 18.0009H21"
                   stroke="#000"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>{" "}
+              </g>{" "}
+              <defs>
+                {" "}
+                <clipPath id="clip0_429_11066">
+                  {" "}
+                  <rect
+                    width="24"
+                    height="24"
+                    fill="white"
+                    transform="translate(0 0.000915527)"
+                  ></rect>{" "}
+                </clipPath>{" "}
+              </defs>{" "}
+            </g>
+          </svg>
+        </button>
+        <button
+          onClick={openNavbar}
+          className="btn btn-ghost btn-square md:hidden"
+        >
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/1000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <g clipPath="url(#clip0_429_11066)">
+                {" "}
+                <path
+                  d="M3 6.00092H21M3 12.0009H21M3 18.0009H21"
+                  stroke="#fff"
                   strokeWidth="2.5"
                   strokeLinecap="round"
                   strokeLinejoin="round"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ type NavbarProps = {
 function Navbar(props: NavbarProps) {
   return (
     <div className={props.displayNavbar}>
-      <div className="w-full py-4 bg-lightgrey absolute z-50">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <div className="flex items-center w-1/2 text-black">
+      <div className="w-full h-20 md:h-32 left-0 py-4 bg-lightgrey absolute z-50 px-10 xl:px-0">
+        <div className="max-w-7xl mx-auto flex justify-between items-start md:items-center">
+          <div className="hidden md:flex items-center w-full lg:w-3/4 xl:w-1/2 text-black">
             <NavLink to="/" className="text-lg font-bold flex items-center">
               <img
                 src={LNlogo}
@@ -22,16 +22,79 @@ function Navbar(props: NavbarProps) {
               />
               LAIA NAVALON
             </NavLink>
-            <NavLink to="/about" className="hover:font-medium ml-32">
+            <NavLink
+              to="/about"
+              className="hover:font-medium md:ml-20 xl:ml-32"
+            >
               ABOUT
             </NavLink>
-            <HashLink to="#contact" smooth className="hover:font-medium ml-36">
+            <HashLink
+              to="#contact"
+              smooth
+              className="hover:font-medium md:ml-28 xl:ml-36"
+            >
               CONTACT
             </HashLink>
           </div>
+          <div className="md:hidden w-full flex items-start justify-between py-2 text-black">
+            <NavLink to="/" className="text-lg font-bold flex items-center">
+              <img
+                src={LNlogo}
+                alt="Laia Navalon Arxe Fullstack Web Developer"
+                className="h-8 pr-8"
+              />
+              LAIA NAVALON
+            </NavLink>
+            <div className="w-1/2 h-screen px-16 flex flex-col absolute right-0 py-14 bg-lightgrey z-40">
+              <NavLink
+                to="/about"
+                onClick={props.closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "font-bold py-4" : "py-4"
+                }
+              >
+                ABOUT
+              </NavLink>
+              <NavLink
+                to="/work"
+                onClick={props.closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "font-bold py-4" : "py-4"
+                }
+              >
+                WORK
+              </NavLink>
+              <NavLink
+                to="/work/commercial"
+                onClick={props.closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "font-bold py-4" : "py-4"
+                }
+              >
+                COMMERCIAL
+              </NavLink>
+              <NavLink
+                to="/work/personal"
+                onClick={props.closeNavbar}
+                className={({ isActive }) =>
+                  isActive ? "font-bold py-4" : "py-4"
+                }
+              >
+                PERSONAL
+              </NavLink>
+              <HashLink
+                to="#contact"
+                smooth
+                onClick={props.closeNavbar}
+                className="hover:font-medium py-4"
+              >
+                CONTACT
+              </HashLink>
+            </div>
+          </div>
           <button
             onClick={props.closeNavbar}
-            className="btn btn-ghost btn-square"
+            className="btn btn-ghost btn-square z-50"
           >
             <svg
               width="50px"
@@ -76,21 +139,23 @@ function Navbar(props: NavbarProps) {
             </svg>
           </button>
         </div>
-        <div className="max-w-7xl h-10 mx-auto">
+        <div className="max-w-7xl h-8 py-1 mx-auto hidden md:block">
           <div className="text-black">
             <NavLink to="/work" className="text-lg font-bold ml-36">
               WORK
             </NavLink>
-            <NavLink to="/work/commercial" className="hover:font-medium ml-32">
+            <NavLink
+              to="/work/commercial"
+              className="hover:font-medium md:ml-20 xl:ml-32"
+            >
               COMMERCIAL
             </NavLink>
-            <HashLink
+            <NavLink
               to="/work/personal"
-              smooth
-              className="hover:font-medium ml-20"
+              className="hover:font-medium md:ml-12 xl:ml-20"
             >
               PERSONAL
-            </HashLink>
+            </NavLink>
           </div>
         </div>
       </div>
