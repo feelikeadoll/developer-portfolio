aba6773523e131b02b84bbce746a78e8434e81ef
Laia Navalon <laianavalon@gmail.com>
2024-09-02 13:04:58 +0200

    Change imported font from Google Fonts to locally installed font - Remove CDN link from index.html, import font in fonts folder to index.css + add it to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      montserrat: ["Avenir Light", "sans-serif"],
+    },
     backgroundSize: {
       full: "100%",
       "50%": "50%",
