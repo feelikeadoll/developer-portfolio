1cd363440aed697b571d429f0ff9bae5da591232
Laia Navalon Arxe <laia.navalon@radissonhotels.com>
2025-09-16 20:27:20 +0200

    update package files and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -68,5 +70,5 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
